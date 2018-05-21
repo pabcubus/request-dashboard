3122fab4f711b6894e1debac954c9ee32a468a2a
Pablo Bassil <pablo.bassil@bairesdev.com>
2018-05-21 01:43:18 -0500

    no message

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { PedidosService } from '../../services/pedidos.service';
 import { SessionService } from '../../services/session.service';
 import { User } from '../../interfaces/user';
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver/FileSaver.min';
+import * as _ from 'lodash';
 
 @Component({
 	selector: 'app-home',
@@ -50,7 +53,40 @@ export class HomeComponent implements OnInit {
 	}
 
 	public exportPedidos(){
+		let pedidos 		= this.queryPedidos();
+		let model			= {};
 
+		let departamentos	= _.groupBy(pedidos, _.property('user.departamento.nombre'));
+
+		for (var key in departamentos) {
+			let array			= [];
+			
+			array.push([
+				{align:'center', text:'PEDIDO ID'},
+				{align:'center', text:'PRODUCTO'},
+				{align:'center', text:'CANTIDAD'},
+				{align:'center', text:'SUBTOTAL'},
+				{align:'center', text:'USUARIO'},
+				{align:'center', text:'DEPARTAMENTO'}
+			]);
+
+			departamentos[key].forEach(pedido => {
+				pedido.detalles.forEach(detalle => {
+					array.push([
+						{align:'center', text: pedido._id},
+						{align:'center', text: detalle.name},
+						{align:'center', text: detalle.cantidad},
+						{align:'center', text: detalle.subtotal},
+						{align:'center', text: pedido.user.nombre+' '+pedido.user.apellido},
+						{align:'center', text: pedido.user.departamento.nombre}
+					]);
+				});
+			});
+
+			model[key] = this._buildXLSTable(array);
+		}
+
+		this.exportToXLSX(model);
 	}
 
 	private queryPedidos(){
@@ -61,7 +97,7 @@ export class HomeComponent implements OnInit {
 			estado: this.filterOptions.estado,
 			usuario: this.filterOptions.usuario
 		};
-		
+
 		if (filterOptions.endDate) filterOptions.endDate.setDate(filterOptions.endDate.getDate() + 1);
 
 		pedidos = pedidos.filter(function(item){
@@ -83,6 +119,56 @@ export class HomeComponent implements OnInit {
 
 	private getUsers(){
 		this.usuarios = this._session.getUsers();
+	}
+
+	private exportToXLSX(content){
+		let wb = XLSX.utils.book_new();
+
+		for (let key in content) {
+			let ws = XLSX.utils.table_to_sheet(content[key], {'raw' : true});
+			XLSX.utils.book_append_sheet(wb, ws, key);
+		}
+
+		let wbout = XLSX.write(wb, {
+			'bookType' : 'xlsx',
+			'type' : 'array'
+		});
+
+		let data = new Blob([wbout],{type:"application/octet-stream"});
+		FileSaver.saveAs(data, "Reporte.xlsx");
+	}
+
+	private _buildXLSTable(array):any{
+		let idWrapper	= 'custom-table-wrapper';
+		let idTable		= idWrapper + '-table';
+
+		let table = document.createElement('table');
+		table.setAttribute('id', idTable);
+
+		let thed = document.createElement('thead');
+
+		table.appendChild(thed);
+
+		let tbdy = document.createElement('tbody');
+		for (let i = 0; i < array.length; i++) {
+			let tr = document.createElement('tr');
+
+			array[i].forEach((item) => {
+				let td = document.createElement('td');
+
+				td.innerHTML			= 'text' in item	? item.text 	: item;
+				td.colSpan				= 'colspan' in item	? item.colspan 	: 1;
+
+				td.setAttribute('align', ('align' in item 	? item.align	: 'left'));
+				tr.appendChild(td);
+			})
+
+			tbdy.appendChild(tr);
+		}
+
+		table.appendChild(tbdy);
+
+		return table;
 	}
 
 	get currentUser():User{
